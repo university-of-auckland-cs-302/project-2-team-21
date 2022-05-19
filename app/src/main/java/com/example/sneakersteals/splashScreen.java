ac854c7703953f64d1ffcec625d04c7a7fbf2f84
ckak912 <ckak912@aucklanduni.ac.nz>
2022-05-19 21:50:31 +1200

    Started xml and Java code for SplashScreen
    Instead of a landing page, going to go with a splash page instead. BRief view that is shown to the user upon startup that will show the logo, app name and something to do with UOA.

---
+++ app/src/main/java/com/example/sneakersteals/splashScreen.java
@@ -0,0 +1,14 @@
+package com.example.sneakersteals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
