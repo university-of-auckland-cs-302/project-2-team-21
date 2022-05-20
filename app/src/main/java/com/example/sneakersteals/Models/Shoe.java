f915c697602a31610597075eb4654aac1e88bfd0
jyou916 <jyou916@aucklanduni.ac.nz>
2022-05-20 18:17:05 +1200

    Added DataProvider Class

--- app/src/main/java/com/example/sneakersteals/Models/Shoe.java
+++ app/src/main/java/com/example/sneakersteals/Models/Shoe.java
@@ -4,24 +4,18 @@ import java.util.List;
 
 public class Shoe {
     String name, brand, description;
-    int id;
     List<String> colourList, imageFilenameList;
     List<Integer> sizeList;
 
     public Shoe (String name, String brand, String description,
-                 int id, List<String> colourList, List<String> imageFilenameList,
+                 List<String> colourList, List<String> imageFilenameList,
                  List<Integer> sizeList) {
         this.name = name;
         this.brand = brand;
         this.description = description;
-        this.id = id;
         this.colourList = colourList;
         this.imageFilenameList = imageFilenameList;
         this.sizeList = sizeList;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public String getName() {
