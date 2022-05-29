3595265d3a11b42fa6121d410a7489ddd16d6260
jyou916 <79771312+jyou916@users.noreply.github.com>
2022-05-29 17:13:50 +1200

    Functionality to swipe between images in DetailsView
    The user can swipe between avaliable images in the detailsview

--- app/src/main/java/com/example/sneakersteals/DetailsActivity.java
+++ app/src/main/java/com/example/sneakersteals/DetailsActivity.java
@@ -9,7 +9,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.sneakersteals.Adaptors.ShoeAdaptor;
+import com.example.sneakersteals.Adaptors.ViewPageAdapter;
 import com.example.sneakersteals.Models.Shoe;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -17,12 +21,14 @@ public class DetailsActivity extends AppCompatActivity {
     class ViewHolder {
         TextView nameTextView, descriptionTextView, sizeTextView;
         ImageView shoeImageView;
+        ViewPager mViewPager;
 
         public ViewHolder() {
             nameTextView = findViewById(R.id.details_name);
             descriptionTextView = findViewById(R.id.details_description);
             sizeTextView = findViewById(R.id.details_size);
-            shoeImageView = findViewById(R.id.details_image);
+            shoeImageView = findViewById(R.id.imageView);
+            mViewPager = findViewById(R.id.viewPager);
         }
     }
 
@@ -44,6 +50,13 @@ public class DetailsActivity extends AppCompatActivity {
         String selectedName = getIntent().getStringExtra("Name");
         Shoe currentShoe = database.getOneShoe(selectedName);
         global.incrementViewCount(currentShoe);
+
+
+        ViewPageAdapter mViewPagerAdapter = new ViewPageAdapter(this, currentShoe.getImageFilenameList());
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        viewPager.setAdapter(mViewPagerAdapter);
+
 
 
         vh.nameTextView.setText(currentShoe.getName());
